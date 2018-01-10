f2d7df2b7bb4a37cd7bd9c451991843c3f124705
iFew <chitpong@gmail.com>
2018-01-11 00:56:01 +0700

    refactoring production code

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -7,21 +7,43 @@ namespace StringCalculator
 
         public int Add(string inputText)
         {
-            if(inputText.Contains(",")) {
-                string[] textArray = inputText.Split(",");
-                
-                int sumNumber = 0;
-                foreach(var textNumber in textArray) {
-                    sumNumber+= int.Parse(textNumber);
-                }
-
-                return sumNumber;
+            if (IsMultipleTextNumber(inputText))
+            {
+                return SumNumber(inputText);
             }
-            
-            if(inputText == "")
+
+            if (IsTextNumberEmpty(inputText))
                 return 0;
 
+            return Number(inputText);
+        }
+
+        private int Number(string inputText)
+        {
             return int.Parse(inputText);
+        }
+
+        private bool IsTextNumberEmpty(string inputText)
+        {
+            return inputText == "";
+        }
+
+        private int SumNumber(string inputText)
+        {
+            string[] textArray = inputText.Split(",");
+
+            int sumNumber = 0;
+            foreach (var textNumber in textArray)
+            {
+                sumNumber += int.Parse(textNumber);
+            }
+
+            return sumNumber;
+        }
+
+        private bool IsMultipleTextNumber(string inputText)
+        {
+            return inputText.Contains(",");
         }
     }
 }
