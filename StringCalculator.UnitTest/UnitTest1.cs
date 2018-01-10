1e2f3ca180bdbfbb1ecb5fe491752c7fb15ae8ec
iFew <chitpong@gmail.com>
2018-01-11 00:08:51 +0700

    added unittest

--- StringCalculator.UnitTest/UnitTest1.cs
+++ StringCalculator.UnitTest/UnitTest1.cs
@@ -1,13 +1,19 @@
 using System;
 using Xunit;
+using StringCalculator;
 
 namespace StringCalculator.UnitTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Input_One_Should_Display_One()
         {
+            Calculator stringCalculator = new Calculator();
+
+            var actual = stringCalculator.add("1");
+
+            Assert.Equal(1, actual);
 
         }
     }
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public object add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
